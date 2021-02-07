0dfef9996e4f324ba22f65c913690279a25e8f26
kouz <lastpuzly@gmail.com>
2021-02-07 20:37:21 +0900

    feat: init demo project from spring.io

---
+++ spring-io/src/test/java/com/example/demobatch/DemoBatchApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.demobatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
