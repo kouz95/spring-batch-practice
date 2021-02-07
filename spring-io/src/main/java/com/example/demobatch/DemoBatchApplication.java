c701749f5c4c0ddf48ca5d26fc2852b77ab34606
kouz <lastpuzly@gmail.com>
2021-02-07 20:48:09 +0900

    feat: implement simple job for csv

--- spring-io/src/main/java/com/example/demobatch/DemoBatchApplication.java
+++ spring-io/src/main/java/com/example/demobatch/DemoBatchApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoBatchApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(DemoBatchApplication.class, args);
+    public static void main(String[] args) throws Exception {
+        System.exit(SpringApplication.exit(SpringApplication.run(DemoBatchApplication.class, args)));
     }
 }
